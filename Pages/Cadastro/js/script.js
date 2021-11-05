f6a7e1cf286af3e8eaf87fecf777698dde81ad4a
Vinícius Kuchnir <vina200902@gmail.com>
2021-11-04 22:17:03 -0300

    Nova estilização da página de cadastro

--- Pages/Cadastro/js/script.js
+++ Pages/Cadastro/js/script.js
@@ -1,9 +1,37 @@
 function AdicionarImagem(){
-    let inputFoto = document.querySelector('#input-foto');
-    let sucessoFoto = document.querySelector('.sucesso-foto');
+    let inputFoto = $('#input-foto');
+    let sucessoFoto = $('.sucesso-foto');
 
     const [file] = inputFoto.files
     if (file) {
        sucessoFoto.style.display = 'block'; 
+    }
+}
+
+function MostrarSenha(e){
+    let input = document.querySelector('#senha');
+
+    if(input.getAttribute('type') == 'password') {
+        input.setAttribute('type', 'text');
+        e.classList.remove('fa-eye');
+        e.classList.add('fa-eye-slash');
+    } else {
+        input.setAttribute('type', 'password');
+        e.classList.remove('fa-eye-slash');
+        e.classList.add('fa-eye');
+    }
+}
+
+function MostarConfirmacaoSenha(e){
+    let input = document.querySelector('#confirmacaoSenha');
+
+    if(input.getAttribute('type') == 'password') {
+        input.setAttribute('type', 'text');
+        e.classList.remove('fa-eye');
+        e.classList.add('fa-eye-slash');
+    } else {
+        input.setAttribute('type', 'password');
+        e.classList.remove('fa-eye-slash');
+        e.classList.add('fa-eye');
     }
 }
