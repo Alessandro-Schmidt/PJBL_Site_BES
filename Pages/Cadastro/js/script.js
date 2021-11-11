e5093ae1a173b1cb37e6150cc816a39b0d518422
Vinícius Kuchnir <vina200902@gmail.com>
2021-11-11 20:36:59 -0300

    Inserção do Cadastro no Banco de Dados

--- Pages/Cadastro/js/script.js
+++ Pages/Cadastro/js/script.js
@@ -30,3 +30,58 @@ function ValidarEmail(email){
         let padraoEmail =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
         return padraoEmail.test(email); 
 }
+
+function VerificarSenhas(senha, senhaConfirmacao){
+    if (senha == senhaConfirmacao && senha.length >= 8 && senhaConfirmacao.length >= 8) {
+        return true;
+    }else{
+        alert("Suas senhas não correspondem ou possuem menos de 8 dígitos")
+        return false;
+    }
+}
+
+function VerificarTipoAcesso(tipoAcesso, codigoValidacao){
+    if (tipoAcesso == 1 && codigoValidacao == '111111') {
+        return true;
+    }else if (tipoAcesso == 2 && codigoValidacao == '222222') {
+        return true;
+    }else if (tipoAcesso == 3 && codigoValidacao == '333333') {
+        return true;
+    }else if (tipoAcesso == 4 && codigoValidacao == '444444') {
+        return true;
+    }else{
+        alert('O seu tipo de acesso não corresponde com o seu PIN');
+        return false;
+    }
+}
+
+function CadastrarUsuario(event){
+    let tipo_acesso = $('.tipo').val(); 
+    let nome = $('#nome').val();
+    let email = $('#email').val();
+    let senha = $('#senha').val();
+    let confirmacaoSenha = $('#confirmacaoSenha').val();
+    let codigoValidacao = $('#codigoValidacao').val();
+
+    if (VerificarTipoAcesso(tipo_acesso, codigoValidacao) && VerificarSenhas(senha, confirmacaoSenha) && ValidarEmail(email) ) {
+        alert("Entrou Ajax");
+        $.ajax({
+			type: "POST",
+			dataType:"json",
+			url: './php/cadastro_usuario.php',
+			async: false,
+			data: {
+				tipo_acesso: tipo_acesso,
+                nome: nome,
+                email: email,
+                senha: senha,
+			},
+			success: function(result){
+                alert(result);
+			}, 
+		})
+    }
+    
+
+}
+
---
+++ Pages/Cadastro/php/cadastro_usuario.php
@@ -0,0 +1,36 @@
+<?php
+    $host = "localhost";
+    $db   = "bes";
+    $user = "root";
+    $pass = "";
+
+    // Inicia a conexão com o banco de dados
+    $conn = mysqli_connect($host, $user, $pass, $db);
+    
+    // Verifica a conexão
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    //Variaveis
+    $tipo_acesso = $_POST['tipo_acesso']; 
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $search = mysqli_query($conn, "SELECT * FROM usuarios WHERE email = '$email'"); 
+    
+    //Verifica no banco de dados se o usuário já existe
+    if(@mysqli_num_rows($search) > 0){
+        echo json_encode("Este usuário já tem um cadastro realizado.");
+    }else{
+        mysqli_query($conn, "INSERT INTO usuarios (nome, email, senha, tipo_acesso) VALUES ('{$nome}', '{$email}', '{$senha}', '{$tipo_acesso}');");
+        echo json_encode("Cadastro realizado com sucesso");
+    }
+
+     
+
+    //Encerra a conexão
+    mysqli_close($conn);
+
+?>
